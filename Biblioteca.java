afb4988b8033c80ac7086f0c847cfe2b6a3858dd
Axum <useraxum@gmail.com>
2023-04-09 16:58:10 -0500

    no

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,7 +1,10 @@
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
-import Clases;
+import Clases.*;
+
+
 public class Biblioteca {
 
     public static void main(String[] args) {
