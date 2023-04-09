1396b82c77a8d032a3d780fb758cbd5814b7dd5c
juan sebastian villarreal angarita <sebasangarit@gmail.com>
2023-04-09 16:46:46 -0500

    Version 1.0

---
+++ Version-1.0/src/clases/Cliente.java
@@ -0,0 +1,42 @@
+package clases;
+import java.util.LinkedList;
+
+public class Cliente {
+    private String nombre;
+    private String clave;
+    private LinkedList<String> listaLibros = new LinkedList<>();
+    public Cliente(String nombre, String clave,LinkedList<String> listaLibros) {
+        this.nombre = nombre;
+        this.clave = clave;
+        this.listaLibros = listaLibros;
+    }
+    public Cliente(){
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    public void mostrarLibros(){
+        for(String i : this.listaLibros){
+            System.out.println(i);
+        }
+    }
+    
+    public LinkedList<String> getListaLibros() {
+        return listaLibros;
+    }
+    
+    
+}
+
