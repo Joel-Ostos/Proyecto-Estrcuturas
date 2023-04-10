d7cb670fefcc164a3cb6878bcb57b7071bae2b7f
joelostoscastro@gmail.com <joelostoscastro@gmail.com>
2023-04-09 21:48:33 -0500

    Casos de prueba, 10 mil, 100 mil, 1 millón, 10 millones y 100 millones, con CRUD

--- Version-1.0/src/clases/LibrosBiblioteca.java
+++ Version-1.0/src/clases/LibrosBiblioteca.java
@@ -1,53 +1,65 @@
 package clases;
 
 import java.util.LinkedList;
-
+import java.util.ArrayList;
 public class LibrosBiblioteca {
-    LinkedList<Libro> libros = new LinkedList<>();
+  LinkedList<Libro> libros = new LinkedList<>();
+  // ArrayList<Libro> libros = new ArrayList<Libro>();
+  public void create(String NombreLibro, String Autor, int CantidadLibros, String Editorial) {
+    Libro libro = new Libro();
+    libro.setNombreLibro(NombreLibro);
+    libro.setAutor(Autor);
+    libro.setCantidadLibros(CantidadLibros);
+    libro.setEditorial(Editorial);
+    libros.add(libro);
+  }
+  public void create(String NombreLibro){
+    for(Libro i : libros){
+      if(i.getNombreLibro().equals(NombreLibro)){
+	System.out.println("Libro en la Biblioteca");
+	System.out.println("guardando libro...");
+      }else{
+	System.out.println("Libro no disponible");
+      }
+    }
+  }
 
-    public void create(String NombreLibro, String Autor, int CantidadLibros, String Editorial) {
-        Libro libro = new Libro();
-        libro.setNombreLibro(NombreLibro);
-        libro.setAutor(Autor);
-        libro.setCantidadLibros(CantidadLibros);
-        libro.setEditorial(Editorial);
-        libros.add(libro);
+  public Libro mostrarPorNombre(String NombreLibro) {
+    for (Libro libro : libros) {
+      if (libro.getNombreLibro().equals(NombreLibro)) {
+	return libro;
+      }
     }
-    public void create(String NombreLibro){
-        for(Libro i : libros){
-            if(i.getNombreLibro().equals(NombreLibro)){
-                System.out.println("Libro en la Biblioteca");
-                System.out.println("guardando libro...");
-            }else{
-                System.out.println("Libro no disponible");
-            }
-        }
+    return null;
+  }
+  public Libro MostrarPorAutor(String Autor){
+    for (Libro libro : libros) {
+      if (libro.getAutor() == Autor)
+	return libro;
     }
+    return null;
+  }
 
-    public Libro mostrarPorNombre(String NombreLibro) {
-        for (Libro libro : libros) {
-            if (libro.getNombreLibro().equals(NombreLibro)) {
-                return libro;
-            }
-        }
-        return null;
+  public LinkedList<String> mostrarTodos() {
+    LinkedList<String> todosLibros = new LinkedList<>();
+    for (Libro libro : libros) {
+      todosLibros.add(libro.getNombreLibro());
     }
+    return todosLibros;
+  }
 
-    public LinkedList<String> mostrarTodos() {
-        LinkedList<String> todosLibros = new LinkedList<>();
-        for (Libro libro : libros) {
-            todosLibros.add(libro.getNombreLibro());
-        }
-        return todosLibros;
+  public String eliminarLibro(String NombreLibro) {
+    for (Libro l : libros) {
+      if (l.getNombreLibro().equals(NombreLibro)) {
+	libros.remove(l);
+	return NombreLibro;
+      }
     }
-
-    public String eliminarLibro(String NombreLibro) {
-        for (Libro l : libros) {
-            if (l.getNombreLibro().equals(NombreLibro)) {
-                libros.remove(l);
-                return NombreLibro;
-            }
-        }
-        return null;
-    }
+    return null;
+  }
+  public String ModificarNombre(String NuevoNombreLibro, String Autor){
+    String Nombre = MostrarPorAutor(Autor).getAutor();
+    MostrarPorAutor(Autor).setNombreLibro(NuevoNombreLibro);
+    return Nombre;
+  }
 }
