045eb285a62351f4f88d8a517940e8b0ccf9be82
Joel-Ostos <joelostoscastro@gmail.com>
2023-04-21 21:07:00 -0500

    Funcion contarlibros

--- Version-1.0/src/clases/LibrosBiblioteca.java
+++ Version-1.0/src/clases/LibrosBiblioteca.java
@@ -4,73 +4,82 @@ import java.util.LinkedList;
 import java.util.TreeSet;
 
 public class LibrosBiblioteca {
-    private TreeSet<Libro> libros;
-    
-    public LibrosBiblioteca() {
-        libros = new TreeSet<>(Comparator.comparing(Libro::getNombre));
+  private TreeSet<Libro> libros;
+
+  public LibrosBiblioteca() {
+    libros = new TreeSet<>(Comparator.comparing(Libro::getNombre));
+  }
+
+  public void create(String nombre, String autor, int cantidad, String editorial) {
+    Libro libro = new Libro(nombre, autor, cantidad, editorial);
+    libros.add(libro);
+  }
+
+  public Libro mostrarPorNombre(String nombre) {
+    for (Libro libro : libros) {
+      if (libro.getNombre().equals(nombre)) {
+	return libro;
+      }
+    }
+    return null;
+  }
+
+  public LinkedList<Libro> mostrarPorAutor(String autor) {
+    LinkedList<Libro> librosPorAutor = new LinkedList<>();
+    for (Libro libro : libros) {
+      if (libro.getAutor().equals(autor)) {
+	librosPorAutor.add(libro);
+      }
+    }
+    return librosPorAutor;
+  }
+
+  public boolean actualizarLibro(String nombre, String nuevoNombre, String nuevoAutor, String nuevaEditorial, String nuevaCategoria) {
+    Libro libro = mostrarPorNombre(nombre);
+    if (libro == null) {
+      return false;
     }
 
-    public void create(String nombre, String autor, int cantidad, String editorial) {
-        Libro libro = new Libro(nombre, autor, cantidad, editorial);
-        libros.add(libro);
-    }
+    libro.setNombre(nuevoNombre);
+    libro.setAutor(nuevoAutor);
+    libro.setEditorial(nuevaEditorial);
+    return true;
+  }
 
-    public Libro mostrarPorNombre(String nombre) {
-        for (Libro libro : libros) {
-            if (libro.getNombre().equals(nombre)) {
-                return libro;
-            }
-        }
-        return null;
+  public Libro eliminarLibro(String nombre) {
+    Libro libro = mostrarPorNombre(nombre);
+    if (libro != null) {
+      libros.remove(libro);
+      return libro;
     }
+    return null;
+  }
 
-    public LinkedList<Libro> mostrarPorAutor(String autor) {
-        LinkedList<Libro> librosPorAutor = new LinkedList<>();
-        for (Libro libro : libros) {
-            if (libro.getAutor().equals(autor)) {
-                librosPorAutor.add(libro);
-            }
-        }
-        return librosPorAutor;
+  public LinkedList<Libro> mostrarPorAutorYLibro(String autor, String nombreLibro) {
+    LinkedList<Libro> librosPorAutorYLibro = new LinkedList<>();
+    for (Libro libro : libros) {
+      if (libro.getAutor().equals(autor) && libro.getNombre().equals(nombreLibro)) {
+	librosPorAutorYLibro.add(libro);
+      }
     }
+    return librosPorAutorYLibro;
+  }
+  public LinkedList<Libro> mostrarTodos(){
+    LinkedList<Libro> librosTodo = new LinkedList<>();
+    for(Libro libro : libros){
+      librosTodo.add(libro);
+    }
+    return librosTodo;
+  }
+  int nLibros = 0;
+  public int CantidadLibros(String NombreLibro){
+    for (Libro l : libros){
+      if (l.getNombreLibro().equals(NombreLibro)){
+	nLibros += 1;
+      }
+    }
+    return nLibros;
+  }
 
-    public boolean actualizarLibro(String nombre, String nuevoNombre, String nuevoAutor, String nuevaEditorial, String nuevaCategoria) {
-        Libro libro = mostrarPorNombre(nombre);
-        if (libro == null) {
-            return false;
-        }
-        
-        libro.setNombre(nuevoNombre);
-        libro.setAutor(nuevoAutor);
-        libro.setEditorial(nuevaEditorial);
-        return true;
-    }
-
-    public Libro eliminarLibro(String nombre) {
-        Libro libro = mostrarPorNombre(nombre);
-        if (libro != null) {
-            libros.remove(libro);
-            return libro;
-        }
-        return null;
-    }
-
-    public LinkedList<Libro> mostrarPorAutorYLibro(String autor, String nombreLibro) {
-        LinkedList<Libro> librosPorAutorYLibro = new LinkedList<>();
-        for (Libro libro : libros) {
-            if (libro.getAutor().equals(autor) && libro.getNombre().equals(nombreLibro)) {
-                librosPorAutorYLibro.add(libro);
-            }
-        }
-        return librosPorAutorYLibro;
-    }
-    public LinkedList<Libro> mostrarTodos(){
-        LinkedList<Libro> librosTodo = new LinkedList<>();
-        for(Libro libro : libros){
-            librosTodo.add(libro);
-        }
-        return librosTodo;
-    }
-
-    // Otros métodos de la clase LibrosBiblioteca
+  // Otros métodos de la clase LibrosBiblioteca
 }
