ed9d2d4bd86c3a1770a2917522f2c354777dad3f
juan sebastian villarreal angarita <sebasangarit@gmail.com>
2023-04-21 20:43:04 -0500

    Modificaciones clase LibrosBiblioteca, adiciones en la clase Libro

--- Version-1.0/src/clases/LibrosBiblioteca.java
+++ Version-1.0/src/clases/LibrosBiblioteca.java
@@ -1,60 +1,76 @@
 package clases;
-
+import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class LibrosBiblioteca {
-  LinkedList<Libro> libros = new LinkedList<>();
-  public void create(String NombreLibro, String Autor, int CantidadLibros, String Editorial) {
-    Libro libro = new Libro(NombreLibro, Autor,CantidadLibros, Editorial);
-    libros.add(libro);
-  }
-  
-  public Libro mostrarPorNombre(String NombreLibro) {
-    for (Libro libro : libros) {
-      if (libro.getNombreLibro().equals(NombreLibro)) {
-	return libro;
-      }
+    private TreeSet<Libro> libros;
+    
+    public LibrosBiblioteca() {
+        libros = new TreeSet<>(Comparator.comparing(Libro::getNombre));
     }
-    return null;
-  }
-  public Libro MostrarPorAutor(String Autor){
-    for (Libro libro : libros) {
-      if (libro.getAutor() == Autor)
-	return libro;
-    }
-    return null;
-  }
 
-  public LinkedList<String> mostrarTodos() {
-    LinkedList<String> todosLibros = new LinkedList<>();
-    for (Libro libro : libros) {
-      todosLibros.add(libro.getNombreLibro());
+    public void create(String nombre, String autor, int cantidad, String editorial) {
+        Libro libro = new Libro(nombre, autor, cantidad, editorial);
+        libros.add(libro);
     }
-    return todosLibros;
-  }
 
-  public String eliminarLibro(String NombreLibro) {
-    for (Libro l : libros) {
-      if (l.getNombreLibro().equals(NombreLibro)) {
-	libros.remove(l);
-	return NombreLibro;
-      }
+    public Libro mostrarPorNombre(String nombre) {
+        for (Libro libro : libros) {
+            if (libro.getNombre().equals(nombre)) {
+                return libro;
+            }
+        }
+        return null;
     }
-    return null;
-  }
-  public String ModificarNombre(String NuevoNombreLibro, String Autor){
-    String Nombre = MostrarPorAutor(Autor).getAutor();
-    MostrarPorAutor(Autor).setNombreLibro(NuevoNombreLibro);
-    return Nombre;
-  }
-  int nLibros = 0;
-  public int CantidadLibros(String NombreLibro){
-    for (Libro l : libros){
-      if (l.getNombreLibro().equals(NombreLibro)){
-	nLibros += 1;
-      }
+
+    public LinkedList<Libro> mostrarPorAutor(String autor) {
+        LinkedList<Libro> librosPorAutor = new LinkedList<>();
+        for (Libro libro : libros) {
+            if (libro.getAutor().equals(autor)) {
+                librosPorAutor.add(libro);
+            }
+        }
+        return librosPorAutor;
     }
-    return nLibros;
-  }
+
+    public boolean actualizarLibro(String nombre, String nuevoNombre, String nuevoAutor, String nuevaEditorial, String nuevaCategoria) {
+        Libro libro = mostrarPorNombre(nombre);
+        if (libro == null) {
+            return false;
+        }
+        
+        libro.setNombre(nuevoNombre);
+        libro.setAutor(nuevoAutor);
+        libro.setEditorial(nuevaEditorial);
+        return true;
+    }
+
+    public Libro eliminarLibro(String nombre) {
+        Libro libro = mostrarPorNombre(nombre);
+        if (libro != null) {
+            libros.remove(libro);
+            return libro;
+        }
+        return null;
+    }
+
+    public LinkedList<Libro> mostrarPorAutorYLibro(String autor, String nombreLibro) {
+        LinkedList<Libro> librosPorAutorYLibro = new LinkedList<>();
+        for (Libro libro : libros) {
+            if (libro.getAutor().equals(autor) && libro.getNombre().equals(nombreLibro)) {
+                librosPorAutorYLibro.add(libro);
+            }
+        }
+        return librosPorAutorYLibro;
+    }
+    public LinkedList<Libro> mostrarTodos(){
+        LinkedList<Libro> librosTodo = new LinkedList<>();
+        for(Libro libro : libros){
+            librosTodo.add(libro);
+        }
+        return librosTodo;
+    }
+
+    // Otros métodos de la clase LibrosBiblioteca
 }
