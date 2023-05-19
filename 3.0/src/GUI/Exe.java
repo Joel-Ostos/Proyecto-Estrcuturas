92c568dbb1ad0bf6f864b21414b8225cc0951bdb
Axum <Axum@DESKTOP-MPA87TQ.(none)>
2023-05-19 00:36:19 -0500

    Version 3.0 con unico frame

---
+++ 3.0/src/GUI/Exe.java
@@ -0,0 +1,203 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package GUI;
+import clases.V1.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
+public class Exe extends javax.swing.JFrame {
+
+    static HashMap<String, String> administrador = new HashMap<>();
+    static HashMap<String, String> credencialesCliente = new HashMap<>();
+    static HashMap<String, Cliente> clientes = new HashMap<>();
+    static LibrosBiblioteca almacen = new LibrosBiblioteca();
+    static ArrayList<Libro> listaSalida;
+    static Cliente cliente;
+    
+    static String Usuario;
+    
+    
+    public Exe() {
+        
+        initComponents();
+        precarga();
+        selInit(1);
+        
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg = new javax.swing.JPanel();
+        panTitle = new javax.swing.JPanel();
+        lblTitle = new javax.swing.JLabel();
+        panContent = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1280, 720));
+        setMinimumSize(new java.awt.Dimension(1280, 720));
+
+        bg.setBackground(new java.awt.Color(255, 255, 255));
+        bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        panTitle.setBackground(new java.awt.Color(255, 150, 50));
+        panTitle.setPreferredSize(new java.awt.Dimension(1280, 80));
+
+        lblTitle.setText("SGB (Sistema de Gestion de Biblioteca)");
+
+        javax.swing.GroupLayout panTitleLayout = new javax.swing.GroupLayout(panTitle);
+        panTitle.setLayout(panTitleLayout);
+        panTitleLayout.setHorizontalGroup(
+            panTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panTitleLayout.createSequentialGroup()
+                .addGap(496, 496, 496)
+                .addComponent(lblTitle)
+                .addContainerGap(549, Short.MAX_VALUE))
+        );
+        panTitleLayout.setVerticalGroup(
+            panTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panTitleLayout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(lblTitle)
+                .addContainerGap(26, Short.MAX_VALUE))
+        );
+
+        bg.add(panTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 70));
+
+        javax.swing.GroupLayout panContentLayout = new javax.swing.GroupLayout(panContent);
+        panContent.setLayout(panContentLayout);
+        panContentLayout.setHorizontalGroup(
+            panContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1280, Short.MAX_VALUE)
+        );
+        panContentLayout.setVerticalGroup(
+            panContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 650, Short.MAX_VALUE)
+        );
+
+        bg.add(panContent, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 1280, 650));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Exe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Exe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Exe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Exe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Exe().setVisible(true);
+            }
+        });
+    }
+
+    
+    
+    
+    
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bg;
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JPanel panContent;
+    private javax.swing.JPanel panTitle;
+    // End of variables declaration//GEN-END:variables
+
+    public void iniciarPanelSesion() {
+        panSesion pb1 = new panSesion();
+        pb1.setSize(1280,650);
+        pb1.setLocation(0,0);
+        panContent.removeAll();
+        panContent.add(pb1);//BorderLayout.CENTER
+        panContent.revalidate();
+        panContent.repaint();
+    }
+    public void iniciarPanelUser() {
+        panUser pb2 = new panUser();
+        pb2.setSize(1280,650);
+        pb2.setLocation(0,0);
+        panContent.removeAll();
+        panContent.add(pb2);//BorderLayout.CENTER
+        panContent.revalidate();
+        panContent.repaint();
+    }
+    public void iniciarPanelAdmin() {
+        panAdmin pb3 = new panAdmin();
+        pb3.setSize(1280,650);
+        pb3.setLocation(0,0);
+        panContent.removeAll();
+        panContent.add(pb3);//BorderLayout.CENTER
+        panContent.revalidate();
+        panContent.repaint();
+       
+    }
+    public void selInit(int i) {
+        if(i == 1){
+            iniciarPanelSesion();
+        }
+        if(i == 2){
+            iniciarPanelUser();
+        }
+        if(i == 3){
+            iniciarPanelAdmin();
+        }
+    }
+    
+    private void precarga(){
+        almacen.create("Libro1", "Gauss", 50, "McGrawHill", "Matematicas");
+        almacen.create("Libro4", "Martinez", 70, "Norma", "Literatura");
+        almacen.create("Libro3", "Dietel", 57, "Norma", "Historia");
+        almacen.create("Libro2", "Gauss", 5, "McGrawHill", "Matematicas");
+        almacen.create("Libro5", "Bonn", 1, "McGrawHill", "Ingenieria");
+        almacen.create("Libro6", "Arrieta", 6, "McGrawHill", "Estadistica");
+        almacen.create("Libro7", "Parrado", 3, "Pearson", "Literatura");
+        almacen.create("Libro8", "Demba Cisse", 4, "McGrawHill", "Literatura");
+        almacen.create("Libro9", "Dietel", 15, "Pearson", "Politica");
+        almacen.create("Libro10", "Gauss", 13, "Norma", "Novela");
+        almacen.create("Libro11", "Gauss", 6, "Pearson", "Estadistica");
+    }
+    
+}
+
+    
---
+++ 3.0/src/V1/LibrosBiblioteca.java
@@ -0,0 +1,234 @@
+package clases.V1;
+
+import clases.V1.Libro;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeSet;
+import java.util.concurrent.CompletionStage;
+import java.util.ArrayList;
+
+public class LibrosBiblioteca {
+
+    private TreeSet<Libro> libros;
+    private Queue<Reserva> reservas;
+    private ArrayList<Libro> listaEnvio;
+
+    public LibrosBiblioteca() {
+        libros = new TreeSet<>(Comparator.comparing(Libro::getNombre)
+                .thenComparing(Libro::getAutor)
+                .thenComparingInt(Libro::getCantidad)
+                .thenComparing(Libro::getEditorial)
+                .thenComparing(Libro::getCategoria));
+        reservas = new LinkedList<>();
+        listaEnvio = new ArrayList<>();
+    }
+
+    public void create(String nombre, String autor, int cantidad, String editorial, String categoria) {
+        Libro libro = new Libro(nombre, autor, cantidad, editorial, categoria);
+        if (libros.contains(libro) != false) {
+            libro.setCantidad(libro.getCantidad() + cantidad);
+        } else {
+            libros.add(libro);
+        }
+
+    }
+
+    public Libro mostrarPorNombre(String nombre) {
+        for (Libro libro : libros) {
+            if (libro.getNombre().equals(nombre)) {
+                
+                System.out.println(libro);
+                return libro;
+            }
+            
+        }
+        return null;
+    }
+
+    public ArrayList mostrarPorAutor(String autor) {
+        if(listaEnvio != null){
+            listaEnvio.clear();
+        }
+        
+        for (Libro libro : libros) {
+            if (libro.getAutor().equals(autor)) {
+                
+                System.out.println(libro);             
+                listaEnvio.add(libro);
+                
+            }
+        }
+        if(listaEnvio == null){
+            return null;
+        }
+        else return listaEnvio;
+    }
+        
+    
+    
+
+    public ArrayList mostrarPorEditorial(String editorial) {
+        if(listaEnvio != null){
+            listaEnvio.clear();
+        }
+        
+        for (Libro libro : libros) {
+            if (libro.getEditorial().equals(editorial)) {
+                
+                System.out.println(libro);             
+                listaEnvio.add(libro);
+                
+            }
+        }
+        if(listaEnvio == null){
+            return null;
+        }
+        
+        else return listaEnvio;
+        
+        
+        
+    }
+    
+    public ArrayList mostrarPorCategoria(String categoria) {
+        if(listaEnvio != null){
+            listaEnvio.clear();
+        }
+
+        for (Libro libro : libros) {
+            if (libro.getCategoria().equals(categoria)) {
+                
+                System.out.println(libro);              
+                listaEnvio.add(libro);
+                
+            }
+        }
+        if(listaEnvio == null){
+            return null;
+        }
+        else return listaEnvio;
+    }
+
+    public int cantidadLibros(String NombreLibro) { //Obsoleto
+        
+        //obsoleto
+        Libro libro = mostrarPorNombre(NombreLibro);
+        return libro.getCantidad();
+    }
+
+    public ArrayList mostrarTodos() {
+        if(listaEnvio != null){
+            listaEnvio.clear();
+        }        
+        for (Libro libro : libros) {
+            System.out.println(libro);             
+            listaEnvio.add(libro);   
+        }
+        
+        if(listaEnvio == null){
+            return null;
+        }
+        else return listaEnvio;
+    }
+
+    public boolean actualizarLibro(String nombre, String nuevoNombre, String nuevoAutor, String nuevaEditorial, String nuevaCategoria) {
+        Libro libro = mostrarPorNombre(nombre);
+        if (libro == null) {
+            return false;
+        }
+
+        libro.setNombre(nuevoNombre);
+        libro.setAutor(nuevoAutor);
+        libro.setEditorial(nuevaEditorial);
+        return true;
+    }
+
+    public Libro eliminarLibro(String nombre) {
+        Libro libro = mostrarPorNombre(nombre);
+        libros.remove(libro);
+        return null;
+    }
+
+    public boolean reservarLibro(String cliente, String libro) {
+        Libro libroEncontrado = mostrarPorNombre(libro);
+
+        if (libroEncontrado == null || libroEncontrado.getCantidad() == 0) {
+            System.out.println("El libro " + libro + " no est� disponible para reservar.");
+            return false;
+        }
+
+        // Reducimos la cantidad de libros disponibles
+        libroEncontrado.setCantidad(libroEncontrado.getCantidad() - 1);
+
+        if (libroEncontrado.getCantidad() > 0) {
+            System.out.println("Libro reservado, Pero quedan " + libroEncontrado.getCantidad() + " copias disponibles.");
+            return true;
+        }
+
+        // Agregamos la reserva a la cola de reservas
+        reservas.add(new Reserva(cliente, libro));
+        System.out.println("Libro reservado con �xito. Ser�s notificado cuando el libro est� disponible.");
+
+        // Verificamos si el libro ya est� disponible
+        String clienteNotificado = notificarCliente(cliente);
+        if (clienteNotificado != null) {
+            System.out.println("El cliente " + clienteNotificado + " ha sido notificado de que el libro " + libro + " est� disponible.");
+        }
+
+        return true;
+    }
+
+    public boolean cancelarReserva(String cliente, String libro) {
+        for (Reserva reserva : reservas) {
+            if (reserva.getCliente().equals(cliente) && reserva.getLibro().equals(libro)) {
+                reservas.remove(reserva);
+                System.out.println("Reserva cancelada...");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void mostrarLibrosReservados() {
+        for (Reserva i : reservas) {
+            System.out.println(i.getLibro());
+        }
+    }
+
+    public String notificarCliente(String cliente) {
+        if (reservas.isEmpty()) {
+            return null;
+        }
+
+        Queue<Reserva> reservasTemporales = new LinkedList<>();
+
+        Reserva reservaCliente = null;
+        while (!reservas.isEmpty()) {
+            Reserva reserva = reservas.poll();
+            if (reserva.getCliente().equals(cliente)) {
+                reservaCliente = reserva;
+                break;
+            }
+            reservasTemporales.add(reserva);
+        }
+
+        while (!reservasTemporales.isEmpty()) {
+            reservas.add(reservasTemporales.poll());
+        }
+
+        if (reservaCliente == null) {
+            // El cliente no tiene reservas en la cola
+            return null;
+        }
+
+        Libro libro = mostrarPorNombre(reservaCliente.getLibro());
+        if (libro != null && libro.getCantidad() > 0) {
+            System.out.println("Notificaci�n: El libro " + libro.getNombre() + " reservado por " + cliente + " est� disponible.");
+            return reservaCliente.getCliente();
+        } else {
+            return null;
+        }
+    }
+
+}
